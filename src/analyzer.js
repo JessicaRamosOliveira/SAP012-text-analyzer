db53fb19e25e21e4721d598ab7afc02bc1c4e61f
Jessica Ramos <jessramos1311@gmail.com>
2023-12-20 09:26:51 -0300

    primeiro

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,28 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const getWordCount = document.querySelector ("word-count");
+    const getWordCount = document.getElementsByName ("user-input");
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  const getCharacterCount = document.querySelector ("character-count");
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+  const getCharacterCountExcludingSpaces = document.querySelector ("character-no-spaces-count");
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  const getAverageWordLength = document.querySelector ("word-length-average");
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  const getNumberCount = document.querySelector ("number-count");
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const getNumberSum = document.querySelector ("number-sum");
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+/*função resetar*/
